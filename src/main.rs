9c4dc849b97f9066cda868d1b6ac6dd3e67408f2
Mike Shearer <shearerbeard@gmail.com>
2022-01-13 22:15:59 -0700

    basic async http fetch and error handle

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,87 @@
-use yew::prelude::*;
+
+
+use reqwasm::http::{Request};
+use serde::Deserialize;
+use yew::{Component, html, Html};
+
+struct TodoApp {
+    // link: ComponentLink<Self>,
+    todos: Option<Vec<Todo>>,
+    // fetch_task: Option<FetchTask>,
+}
+
+#[derive(Deserialize, Clone, PartialEq, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Todo {
+    pub user_id: u64,
+    pub id: u64,
+    pub title: String,
+    pub completed: bool
+}
 
 enum Msg {
-    AddOne,
+    MakeReq,
+    Resp(Result<Vec<Todo>, reqwasm::Error>),
 }
 
-struct Model {
-    value: i64,
-}
-
-
-impl Component for Model {
+impl Component for TodoApp {
     type Message = Msg;
     type Properties = ();
 
-    fn create(_ctx: &Context<Self>) -> Self {
+    fn create(ctx: &yew::Context<Self>) -> Self {
+        ctx.link().send_message(Msg::MakeReq);
         Self {
-            value: 0,
+            todos: None,
         }
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+    fn update(&mut self, ctx: &yew::Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::AddOne => {
-                self.value += 1;
+            Msg::MakeReq => {
+                let link = ctx.link().clone();
+
+                wasm_bindgen_futures::spawn_local(async move {
+                    let req = Request::get("https://jsonplaceholder.typicode.com/todos");
+                    let resp = req.send().await.unwrap();
+                    let result: Result<Vec<Todo>, reqwasm::Error> = resp.json().await;
+
+                    link.send_message(Msg::Resp(result));
+                });
+                true
+            }
+
+            Msg::Resp(resp) => {
+                if let Ok(data) = resp {
+                    self.todos = Some(data);
                     true
+                } else {
+                    false
+                }
+
             }
         }
     }
 
-    fn view(&self, ctx: &Context<Self>) -> Html {
+    fn view(&self, ctx: &yew::Context<Self>) -> Html {
         let link = ctx.link();
+
+        let todo_list = match &self.todos {
+            Some(todos) => todos.clone(),
+            None => vec![]
+        };
 
         html! {
             <div>
-                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
-                <p>{ self.value }</p>
+                <h1>{ "Hello World!" }</h1>
+                <button onclick={link.callback(|_| Msg::MakeReq)}></button>
+            { todo_list.into_iter().map(|_| {
+                html! { <p>{ "1" }</p> }
+            }).collect::<Html>() }
             </div>
         }
     }
 }
 
 fn main() {
-    yew::start_app::<Model>();
+    yew::start_app::<TodoApp>();
 }
